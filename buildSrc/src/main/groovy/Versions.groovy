0bd5b3331246e15038066d485da91f1c2230b51f
Dirk Duehr <d.duehr@enbw.com>
2024-06-07 18:57:03 +0200

    build.gradle.kts reorganized

---
+++ buildSrc/src/main/groovy/Versions.groovy
@@ -0,0 +1,12 @@
+interface Versions {
+    String kotlinJvmTarget = "21"
+
+    // Plugins
+    String springBootPluginVersion = "3.3.0"
+    String springDependencyManagementPluginVersion = "1.1.5"
+    String kotlinJvmPluginVersion = "1.9.24"
+    String kotlinSpringPluginVersion = "1.9.24"
+
+    // Dependencies
+    String springDocVersion = "2.5.0"
+}
